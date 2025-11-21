b86ac264452b99fa449221fc49a7165e60f1ac5a
pasindu <pasindulakshanexample04@gmail.com>
2025-11-21 16:29:40 +0530

    update code

---
+++ mybatch5/index.js
@@ -0,0 +1,14 @@
+//name module import
+import { add } from "/math.js";
+//method module import
+import message from "/message.js";
+const user = {
+  name: "pasindu",
+  email: "pasindu@gmail.com",
+};
+console.log(user);
+console.log("Add 2+3=", add(2, 3));
+console.log("pasindu", "22");
+let Bignumber = 1399732038131818131557557597858750875075757587486864858n;
+console.log(Bignumber);
+console.log("type of",typeof Bignumber);
