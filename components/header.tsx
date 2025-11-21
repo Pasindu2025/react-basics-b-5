22dfff7d5a54e690fd48a51e5c0b5ec48f3e4a5f
pasindu <pasindulakshanexample04@gmail.com>
2025-11-21 16:07:53 +0530

    this week

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+//reusble components of header
+export default function Header() {
+  return (
+    <header className=" flex-1 h-16 bg-amber-100 font-black justify-center text-center">
+      This is my header
+      <div className="container flex h-16 items-center"></div>
+    </header>
+  );
+}
---
+++ components/mainnav.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Logo } from "@/components/logo";
+import { ModeTrogle } from "@/components/mod-trgal";
+export function MainNav() {
+  return (
+    <header className="border-primary/20 bg-background sticky top-0 z-10 w-full border-b">
+      <div className="container max-w-[87.5rem] px-8 flex h-12 px-36 items-center">
+        <Link href="/" className="flex items-center gap-2-">
+          <Logo />
+          <span className="text-primary text-xl font-bold ml-2">Cinescope</span>
+        </Link>
+        <nav  className="ml-auto flex items-center gap-4"  >
+          <Link href="/Movies">Movies</Link>
+          <Link href="/Movies">Genras</Link>
+          <Link href="/Movies">About</Link>
+          <Link href="/Movies">Admin</Link>
+        </nav>
+        <ModeTrogle />
+      </div>
+    </header>
+  );
+}
